0ded9b7c7c869442aabd808364b6abcb39ed799c
mcleddy <maryleddy1@gmail.com>
2019-11-24 17:04:40 -0700

    added css

---
+++ assets/script.js
@@ -0,0 +1,90 @@
+
+var config = {
+    apiKey: "AIzaSyAuW4LPEIfCCkBKR0uw-HfaAuROJ-l89rA",
+    authDomain: "trainscheduling-73f08.firebaseapp.com",
+    databaseURL: "https://trainscheduling-73f08.firebaseio.com/trainName/mOSp8Z7iHIyATMxE8i4D",
+    storageBucket: "gs://trainscheduling-73f08.appspot.com"
+
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+//onclick function variables
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+   var trainName = $("#trainName").val().trim();
+   var trainDestination = $("#trainDestination").val().trim();
+   var firstTrain = $("#firstTrain").val().trim();
+   var frequency = $("#frequency").val().trim();
+
+
+
+    //push to database
+    var newTrain = {
+        trainName: trainName,
+        trainDestination: trainDestination,
+        firstTrain: firstTrain,
+        frequency: frequency,
+    };
+
+    database.ref().push(newTrain);
+    //reset forms
+    console.log(newTrain.trainName);
+    console.log(newTrain.trainDestination);
+    console.log(newTrain.FirstTrain);
+    console.log(newTrain.frequency);
+
+    ///clear boxes
+    $("#trainName").val("")
+    $("#trainDestination").val("")
+    $("#trainTime").val("")
+    $("#frequency").val("")
+});
+
+
+// create a database event
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+    ///bariable
+    var tName = childSnapshot.val().trainName;
+    var tDestionation = childSnapshot.val().trainDestination;
+    var tFrequency = childSnapshot.val().frequency;
+    var tFirstTrain = childSnapshot.val().firstTrain;
+
+    var timeArray = tFirstTrain.split(":");
+    var trainTime = moment().hours(timeArray[0]).minutes(timeArray[1]);
+    var maxMoment = moment.max(moment().trainTime);
+    var tMinutes;
+    var tArrival;
+
+    if (maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+
+    } else {
+
+        var differentTimes = moment().diff(trainTime, "minutes");
+        var tRemainder = differentTimes % tFrequency;
+        tMinutes = tFrequency - tRemainder;
+        tArrival = moment()
+            .add(tMinutes, "m")
+            .format("hh: mm A");
+    }
+    console.log(tMinutes);
+    console.log(tArrival);
+
+    //dump new infointo table
+    $("#tableContent >tbody").append(
+        $("<tr>").append(
+            $("<td>").text(tName),
+            $("<td>").text(tDestionation),
+            $("<td>").text(tFrequency),
+            $("<td>").text(tArrival),
+            $("<td>").text(tMinutes),           
+        )
+    );
+});
